7109f63b52b0de82b659c8fdae1e5f75169c683a
lipd <lipeidao@gmail.com>
2021-05-28 18:34:39 +0800

    feat: 配置 dev server

--- scripts/config/webpack.common.js
+++ scripts/config/webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { isDevelopment, PROJECT_PATH } = require('../constants')
 
 module.exports = {
@@ -9,4 +10,27 @@ module.exports = {
     filename: `js/[name]${isDevelopment ? '' : '.[hash:8]'}.js`,
     path: path.resolve(PROJECT_PATH, './dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(PROJECT_PATH, './public/index.html'),
+      filename: 'index.html',
+      cache: false,
+      minify: isDevelopment
+        ? false
+        : {
+            removeAttributeQuotes: true,
+            collapseWhitespace: true,
+            removeComments: true,
+            collapseBooleanAttributes: true,
+            collapseInlineTagWhitespace: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            minifyCSS: true,
+            minifyJS: true,
+            minifyURLs: true,
+            useShortDoctype: true,
+          },
+    }),
+  ],
 }
--- scripts/config/webpack.development.js
+++ scripts/config/webpack.development.js
@@ -1,6 +1,16 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
+const { SERVER_HOST, SERVER_PORT } = require('../constants')
 
 module.exports = merge(common, {
   mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    host: SERVER_HOST,
+    port: SERVER_PORT,
+    clientLogLevel: 'silent', // 日志等级
+    compress: true, // 是否启用 gzip 压缩
+    open: true, // 打开默认浏览器
+    hot: true, // 热更新
+  },
 })
