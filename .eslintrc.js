b91034b299cc3074e112d3a26be2a053f0f6480e
lipd <lipeidao@gmail.com>
2021-05-28 18:18:57 +0800

    feat: 规范工程

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+const OFF = 0
+// const WARN = 1
+const ERROR = 2
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:promise/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'unicorn', 'promise', 'prettier', '@typescript-eslint'],
+  rules: {
+    'import/extensions': [
+      ERROR,
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+        jsx: 'never',
+        json: 'never',
+      },
+    ],
+    'import/no-extraneous-dependencies': [ERROR, { devDependencies: true }],
+    'import/prefer-default-export': OFF,
+    'import/no-unresolved': ERROR,
+    'import/no-dynamic-require': OFF,
+    'unicorn/prefer-module': OFF,
+    'unicorn/filename-case': [
+      ERROR,
+      {
+        cases: {
+          // 中划线
+          kebabCase: true,
+          // 小驼峰
+          camelCase: false,
+          // 下划线
+          snakeCase: false,
+          // 大驼峰
+          pascalCase: false,
+        },
+      },
+    ],
+    'react/prop-types': OFF,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.tsx', '.ts', '.js', 'jsx', '.json'],
+      },
+    },
+  },
+}
