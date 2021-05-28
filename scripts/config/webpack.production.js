f8db50bd9349a0e4563166413b29fb058886440e
lipd <lipeidao@gmail.com>
2021-05-28 19:19:28 +0800

    feat: 自动清理 dist

--- scripts/config/webpack.production.js
+++ scripts/config/webpack.production.js
@@ -1,7 +1,8 @@
 const { merge } = require('webpack-merge')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const common = require('./webpack.common')
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'none',
+  plugins: [new CleanWebpackPlugin()],
 })
