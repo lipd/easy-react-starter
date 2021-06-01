dcc087e6a206d7877cdf54c0f5e44b58db5fc924
lipd <lipeidao@gmail.com>
2021-06-02 00:27:27 +0800

    feat: 配置代理

--- scripts/config/webpack.development.js
+++ scripts/config/webpack.development.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge')
 const webpack = require('webpack')
 const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const { proxySetting } = require('../proxy')
 const common = require('./webpack.common')
 const { SERVER_HOST, SERVER_PORT } = require('../constants')
 
@@ -14,6 +15,9 @@ module.exports = merge(common, {
     compress: true, // 是否启用 gzip 压缩
     open: true, // 打开默认浏览器
     hot: true, // 热更新
+    proxy: {
+      ...proxySetting,
+    },
   },
   plugins: [new ReactRefreshPlugin(), new webpack.HotModuleReplacementPlugin()],
   target: 'web', // 必须设置，这样才会自动刷新
