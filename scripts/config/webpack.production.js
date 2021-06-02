bf8a81d557af7b62455391c2f30467ee1b489a28
lipd <lipeidao@gmail.com>
2021-06-02 16:41:58 +0800

    feat: 打包后进行分析

--- scripts/config/webpack.production.js
+++ scripts/config/webpack.production.js
@@ -6,6 +6,7 @@ const path = require('path')
 const glob = require('glob')
 const PurgeCSSPlugin = require('purgecss-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const common = require('./webpack.common')
 const { PROJECT_PATH } = require('../constants')
 
@@ -19,6 +20,11 @@ module.exports = merge(common, {
     new webpack.BannerPlugin({
       raw: true,
       banner: '/** @preserve Powered by easy-react-starter (https://github.com/lipd/easy-react-starter) */',
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'server',
+      analyzerHost: '127.0.0.1',
+      analyzerPort: 8888,
     }),
   ],
   devtool: false,
