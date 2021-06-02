68a5527a41ce19dd7e107abcf01ccccb48b6b401
lipd <lipeidao@gmail.com>
2021-06-02 13:54:48 +0800

    feat: 去除没有使用的 css 代码

--- scripts/config/webpack.production.js
+++ scripts/config/webpack.production.js
@@ -1,10 +1,19 @@
 const { merge } = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path')
+const glob = require('glob')
+const PurgeCSSPlugin = require('purgecss-webpack-plugin')
 const common = require('./webpack.common')
+const { PROJECT_PATH } = require('../constants')
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${path.resolve(PROJECT_PATH, './src')}/**/*.{tsx,scss,less,css}`, { nodir: true }),
+    }),
+  ],
   devtool: false,
   optimization: {
     splitChunks: {
